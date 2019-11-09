da1c33588b5302f8355e56aef00ce22dc2f40b7f
kjh03160 <49090888+kjh03160@users.noreply.github.com>
2019-11-09 21:51:58 +0900

    닉네임 1글자 조건 추가

--- 6.py
+++ 6.py
@@ -2,6 +2,8 @@ def solution(forms):
     answer = []
     for i in range(len(forms) - 1):
         for j in range(len(forms[i][1]) - 1):
+            if len(forms[i][1]) == 1:
+                break
             a = forms[i][1][j:j+2]
             for k in range(i + 1, len(forms)):
                 if a in forms[k][1] and not forms[k][0] in answer:
